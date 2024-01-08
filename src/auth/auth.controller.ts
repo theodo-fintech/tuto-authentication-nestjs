637dd22406af3ced086c7f9eb219f2fde7e750c4
camillefauchier <camillef@sipios.com>
2024-01-08 17:19:00 +0100

    feat: add register endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,17 @@
 import {
   BadRequestException,
+  Body,
   Controller,
   Post,
   Request,
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LoginResponseDTO } from './dtos/login-response.dto';
+
 import { AuthGuard } from '@nestjs/passport';
+import { RegisterRequestDto } from './dtos/register-request.dto';
+import { LoginResponseDTO } from './dtos/login-response.dto';
+import { RegisterResponseDTO } from './dtos/register-response.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -17,5 +21,12 @@ export class AuthController {
   @Post('login')
   async login(@Request() req): Promise<LoginResponseDTO | BadRequestException> {
     return this.authService.login(req.user);
+  }
+
+  @Post('register')
+  async register(
+    @Body() registerBody: RegisterRequestDto,
+  ): Promise<RegisterResponseDTO | BadRequestException> {
+    return await this.authService.register(registerBody);
   }
 }
