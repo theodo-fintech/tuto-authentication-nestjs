2e47d496aaee82258b6508157fee8fffa47a81f6
camillefauchier <camillef@sipios.com>
2024-01-08 16:26:25 +0100

    feat: add validateUser method in AuhtService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,27 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 import { User } from 'src/users/users.entity';
 import { AccessToken } from './types/AccessToken';
+import { UsersService } from 'src/users/users.service';
 @Injectable()
 export class AuthService {
   constructor(
+    private usersService: UsersService,
     private jwtService: JwtService,
   ) {}
 
+  async validateUser(email: string, password: string): Promise<User> {
+    const user: User = await this.usersService.findOneByEmail(email);
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+    const isMatch: boolean = bcrypt.compareSync(password, user.password);
+    if (!isMatch) {
+      throw new BadRequestException('Password does not match');
+    }
+    return user;
+  }
 
   async login(user: User): Promise<AccessToken> {
     const payload = { email: user.email, id: user.id };
