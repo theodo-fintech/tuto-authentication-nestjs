d028e282461a79ab2b5964bfe6234954ad5a24b1
Camille Fauchier <camillefauchier@ip-192-168-1-16.eu-west-3.compute.internal>
2025-07-20 21:38:59 +0200

    fix: fix broken test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
+    const mockUsersService = {
+      findOneById: jest.fn().mockResolvedValue({
+        id: 1,
+        email: 'test@test.com',
+        firstName: 'World',
+      }),
+    };
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World!"', async () => {
+      const mockUser = { id: 1 };
+      const result = await appController.getHello(mockUser);
+      expect(result).toBe('Hello World!');
     });
   });
 });
