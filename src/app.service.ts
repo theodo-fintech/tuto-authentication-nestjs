9ea70f56b8d17e2d505ca65ca65f30817b395039
Camille Fauchier <camillefauchier@ip-192-168-1-16.eu-west-3.compute.internal>
2025-07-22 21:19:05 +0200

    refactor: update authentication services to use Prisma types

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from './users/users.service';
-import { User } from './users/users.entity';
-import { UUID } from 'crypto';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AppService {
   constructor(private usersService: UsersService) {}
-  async getHello(userId: UUID): Promise<string> {
+  async getHello(userId: number): Promise<string> {
     const user: User = await this.usersService.findOneById(userId);
     return `Hello ${user.firstName}!`;
   }
