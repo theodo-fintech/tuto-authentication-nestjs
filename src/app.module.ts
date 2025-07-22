95bcde7c1a3f60767bacf18fa0934ef8b3afc756
Camille Fauchier <camillefauchier@ip-192-168-1-16.eu-west-3.compute.internal>
2025-07-22 21:20:42 +0200

    feat: complete TypeORM to Prisma migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/users.entity';
 import { UsersModule } from './users/users.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtGuard } from './auth/guards/jwt.guard';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USER'),
-        database: configService.get<string>('DB_NAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        entities: [User],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
+    PrismaModule,
     UsersModule,
     AuthModule,
   ],
